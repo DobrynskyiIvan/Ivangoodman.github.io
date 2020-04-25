22f9b5ac80da637575886f7ca3569420f37bb331
ivan <ivangoodman36@gmail.com>
2020-04-25 18:14:41 +0300

    final  task of class done

---
+++ lesson28/js/main.js
@@ -0,0 +1,122 @@
+//Реализовать класс, описывающий окружность.
+class Circle {
+    constructor(radiusValue) {
+        this.radius = radiusValue;
+    }
+    get radius() {
+        return this._radius;
+    }
+    set radius(value) {
+
+        this._radius = value;
+    }
+    get diametr() {
+        let diametr = 2 * this.radius
+        return diametr;
+    }
+    squareCircle() {
+        let square = Math.PI * (this.radius ** 2) / 4;
+        return ` This is square: ${square}`;
+
+    }
+    lengthCircle() {
+        let length = 2 * Math.PI * this.radius;
+        return `this is length of circle: ${length}`;
+    }
+
+}
+let one = new Circle(5);
+
+console.log(one.diametr);
+console.log(one.radius);
+console.log(one.lengthCircle());
+console.log(one.squareCircle());
+
+
+// Task --> 2
+class Marker {
+    color = "Red";
+    colorLeftInPercent = 5;
+    // method that write text and set how much color left  inside
+    writeText(string) {
+        string = string.replace(/\s+/g, '');
+        const letter = 0.5;
+
+        for (let i = 0; i < string.length; i++) {
+            if ((this.colorLeftInPercent < letter)) {
+                console.log("this is the \n end of color");
+                break;
+            }
+            this.colorLeftInPercent = this.colorLeftInPercent - (letter)
+            console.log(string[i]);
+        }
+
+    }
+
+}
+// new class that refill marker
+class MarkerLoader extends Marker {
+    //mathod that sets new level of color
+    refill(colorRefill) {
+        this.colorLeftInPercent += colorRefill;
+        console.log(`Marker was refilded till ${this.colorLeftInPercent} persents`)
+    }
+};
+// object of new marker
+let newMarker = new MarkerLoader();
+
+newMarker.writeText("Cxvfbv");
+newMarker.refill(20);
+
+// Task  ---> 3
+
+//clas for employee
+class Employee {
+    constructor(name, surname, age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+}
+// array of employees
+let bankEmployee = [{
+    name: "John",
+    surname: "Smit",
+    age: 30
+}, {
+    name: "Garry",
+    surname: "Teilor",
+    age: 20
+}, {
+    name: "Baron",
+    surname: "O'braiant",
+    age: 25
+}];
+// class impliment bank
+class EmpTable {
+    constructor(arrEmp) {
+        this.bankEmployee = arrEmp
+
+    }
+    getHtml() {
+        document.write(`<table > <thead>
+        <tr>
+            <th>Name</th>
+            <th> Surname</th>
+            <th>Age</th>
+        </tr>
+    </thead>
+    <tbody>`)
+        for (let i of this.bankEmployee) {
+            document.write(`   <tr  >
+            <td> ${i.name}</td>
+            <td>${i.surname}</td>
+            <td> ${i.age}</td>
+        </tr>`)
+        }
+        document.write(` </tbody>
+        </table>`)
+    }
+}
+let Abank = new EmpTable(bankEmployee);
+Abank.getHtml();
