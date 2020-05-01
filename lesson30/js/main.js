44d62a953745c4368cc560953e962b4170526f49
ivan <ivangoodman36@gmail.com>
2020-05-01 13:06:37 +0300

    second  task of events  done

--- lesson30/js/main.js
+++ lesson30/js/main.js
@@ -1,4 +1,4 @@
-/// Task ------->  1
+/// ===============================       Task ------->  1 =============================================
 let page = document.getElementById("task1");
 let textfield = document.getElementById("textfield");
 
@@ -19,11 +19,94 @@ document.addEventListener('keydown', function (event) {
 
     }
 });
+//=============== END OF TASK 1 =======================
 
 
+// ===========         Task 2         =============
+// clas of object User
+class User {
+    constructor(name, age, country) {
+        this.name = name;
+        this.country = country;
+        this.age = age;
+    }
+};
+// list of users
+let usersList = [
+    new User("Maria Anders", 31, "Germany"),
+    new User("Francisco Chang", 23, "Mexico"),
+    new User("Roland Mendel", 26, "Austria"),
+    new User("Helen Bennett ", 30, "UK"),
+    new User("Yoshi Tannamuri", 28, "Canada"),
+    new User("Giovanni Rovelli", 25, "Italy")
+]
+// placeHolder for list 
+let table = document.body.children[1].children[1];
 
-document.addEventListener("click", function (event) {
-    console.log(event.target);
-    console.log(event)
+//class for list description  and sort
+class UserTable {
+    constructor(arrUsr, plaseHolder) {
+        this.userArray = arrUsr;
+        this.plaseHolder = plaseHolder;
+
+    }
+    getHtml() {
+        this.plaseHolder.innerHTML = "";
+        for (let i of this.userArray) {
+            this.plaseHolder.innerHTML += `   <tr  >
+            <td> ${i.name}</td>
+            <td>${i.age}</td>
+            <td> ${i.country}</td>
+        </tr>`;
+        }
+
+    }
+    sortInArray(itemName) {
+        this.userArray.sort(function (a, b) {
+            if (a[itemName] > b[itemName]) {
+                return 1;
+            }
+            if (a[itemName] < b[itemName]) {
+                return -1;
+            }
+            // a должно быть равным b
+            return 0;
+        });
+
+        this.userArray.forEach(element => {
+            console.log(element)
+        });
+    }
+
+
+}
+
+
+// new table that contains users in table
+let Agroup = new UserTable(usersList, table);
+Agroup.getHtml();
+
+let age = document.getElementById("userAge");
+age.addEventListener('click', function (event) {
+
+    Agroup.sortInArray("age");
+    Agroup.getHtml();
 
 });
+
+let user = document.getElementById("userName");
+user.addEventListener('click', function (event) {
+
+    Agroup.sortInArray("name");
+    Agroup.getHtml();
+
+});
+let country = document.getElementById("userCountry");;
+country.addEventListener('click', function (event) {
+
+    Agroup.sortInArray("country");
+    Agroup.getHtml();
+
+});
+
+//=======================  END OF TASK 2    =========================
