5f7c92f7d0fcce53f15b492a071bb18aa04423c0
ivan <ivangoodman36@gmail.com>
2020-05-09 15:53:03 +0300

    task AJAX allmoust finished

--- lesson33/js/index.js
+++ lesson33/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    //start searching
     $(".submit").click(function (e) {
         e.preventDefault();
         let name = $(".inputValue").val();
@@ -7,48 +8,81 @@ $(document).ready(function () {
 
         $.getScript('js/api/index.js', function () {
             setUrl(name, type)
-            ajaxCall(setSliderItems);
+            ajaxCall(setFilmItems, pagesSelect);
         });
 
     });
 
 
-    // $(".next").click(function (e) {
-    //     e.preventDefault();
-    //     $.getScript('js/api/index.js', function () {
-    //         setUrl(name, page)
-    //         ajaxCall(setSliderItems);
-    //     });
-
-    // });
-
-    function setSliderItems(data) {
+    function setFilmItems(data) {
         let list = data.Search
+        console.log(list)
         let obj = list.map(item => {
-            return `<div class="item"> <img  src="${item.Poster}"> </div>   `;
+            return `<div  class="item"> <img id="${item.imdbID}"  alt="${item.Title}"  src="${item.Poster}"> </div>   `;
         });
-        // ${item.Title} 
+        $('.display').append(obj.join(''));
+    };
 
-        $('.dispay').append(obj.join(''));
-        // console.log(data)
-        // console.log(obj)
-        // console.log(data.Search)
-        let pagesCount = Math.round(data.totalResults / 10);
-        for (let i = 0; i < pagesCount; i++) {
-            $('.countOfPages').append(` <option value="${i}">${i}</option>`);
-        }
-    }
-
-
+    //show pages when select 
     $(".countOfPages").change(function (e) {
         e.preventDefault();
+        $('.display').empty();
         console.log(e.target.value)
-        page = e.target.value;
+        let page = e.target.value;
         $.getScript('js/api/index.js', function () {
             setPage(page)
             ajaxCall(setSliderItems);
         })
+    });
+
+    function pagesSelect(data) {
+        let pagesCount = Math.round(data.totalResults / 10);
+        $('.countOfPages').empty();
+        for (let i = 1; i <= pagesCount; i++) {
+            $('.countOfPages').append(` <option value="${i}">${i}</option>`);
+        }
+    }
+    /// show direct film
+    function filmSelect(item) {
+        let info = `  
+        <div class="short">
+        <a target="_blank" class="fancybox"
+            href=" ${item.Poster}">
+            <img src=" ${item.Poster}"
+                itemprop="image" class="poster poster-tooltip">
+        </a>
+    </div>
+    <div class="full">
+        <div class="top-date">Released: ${item.Released} </div>
+        <div class="name-block">
+            <h2 class="name" itemprop="name"><a class="btn-tooltip">Title: ${item.Title}  </a></h2>
+            <div class="origin-name">Runtime: ${item.Runtime} </div>
+        </div>
+        <div class="itemSel category"> Genre: ${item.Genre} </div>
+        <div class="itemSel year"> Year: ${item.Year}
+        </div>
+        <div class="itemSel rating"> imdbRating: ${item.imdbRating}
+        </div>
+
+        <div class="itemSel translate"> Language: ${item.Language} </div>
+        <div class="itemSel actors"> Director: ${item.Director}</div>
+        <div class="itemSel actors"> Actors: ${item.Actors}
+        </div>
+        <p itemprop="description">Plot: ${item.Plot} </p>
+
+    </div>`;
+        $('.filmDetails').empty();
+        $('.filmDetails').append(info);
+    }
+    $('.display').click(function (e) {
+        console.log(e.target.id)
+        $.getScript('js/api/index.js', function () {
+            setFilm(e.target.id)
+            ajaxCall(filmSelect);
+        })
+
 
 
     });
+
 });
