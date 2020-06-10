32dc9acf9eb5e153cab5c895933d9a8bb735ddc3
Ivangoodman <ivangoodman36@gmail.com>
2020-06-10 03:40:01 +0300

    main part of worck done

--- Classes/WeatherForecast/src/components/Date/Time.component.js
+++ Classes/WeatherForecast/src/components/Date/Time.component.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 function Time(props) {
-    let  dateObj = new Date(props.sec * 1000);
+  let dateObj = new Date(props.sec * 1000);
   let hours = dateObj.getUTCHours();
   let minutes = dateObj.getUTCMinutes();
   let seconds = dateObj.getSeconds();
-  let timeString = hours.toString().padStart(2, '0') + ':' + 
-  minutes.toString().padStart(2, '0') 
-  
-  return <span className="time">{timeString } </span> }
-  
-export  default  Time
- 
+  let timeString =
+    hours.toString().padStart(2, "0") +
+    ":" +
+    minutes.toString().padStart(2, "0");
+
+  return <span className="time">{timeString} </span>;
+}
+
+export default Time;
